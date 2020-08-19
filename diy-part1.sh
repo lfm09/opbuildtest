31e920c04fcc502604f7f286a8fd82ff1274a4e3
lfm09 <69192865+lfm09@users.noreply.github.com>
2020-08-19 02:13:49 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
+#sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/lfm09/openwrt-package' feeds.conf.default
-sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
-sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/lfm09/Lienol-openwrt-package' feeds.conf.default
