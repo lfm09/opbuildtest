43de24c7cfadd5a2bfea0ebf865e3a82b0bb3a4e
lfm09 <69192865+lfm09@users.noreply.github.com>
2020-09-08 00:19:57 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/lfm09/Lienol-openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
