4e86482be267da24526c4e1fe6242920ec81f02b
lfm09 <69192865+lfm09@users.noreply.github.com>
2020-09-13 00:38:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i '/argon_dark_purple/d' feeds/lienol/lienol/luci-theme-argon-dark-mod/root/etc/uci-defaults/30_luci-theme-argon-dark-mod
-rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/luasrc/view/themes/argon_dark_purple
-rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/htdocs/luci-static/argon_dark_purple
-rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/htdocs/luci-static/argon_dark/img
+#sed -i '/argon_dark_purple/d' feeds/lienol/lienol/luci-theme-argon-dark-mod/root/etc/uci-defaults/30_luci-theme-argon-dark-mod
+#rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/luasrc/view/themes/argon_dark_purple
+#rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/htdocs/luci-static/argon_dark_purple
+#rm -rf feeds/lienol/lienol/luci-theme-argon-dark-mod/htdocs/luci-static/argon_dark/img
